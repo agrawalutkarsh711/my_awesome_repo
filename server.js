03f19333aec0556e07c40ada8b166c8437fa240e
unknown <agrawalutkarsh711@gmail.com>
2020-11-21 17:17:00 +0530

    Database added
    Signed-off-by: unknown <agrawalutkarsh711@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+
+const {db , Tasks} = require('./db.js')
+
 const PORT = process.env.PORT || 4444
 const app = express()
 
@@ -6,6 +9,22 @@ app.get('/',(req,res)=>{
     res.send('HELLO TO ALL')
 })
 
+app.get('/tasks',(req,res)=>{
+    const tasks = Tasks.findAll();
+    res.send(tasks)
+})
+
+app.post('/tasks',(req,res)=>{
+    const task = Tasks.create(req.body)
+    res.send(task)
+})
+
 app.use('/public',express.static(__dirname + "/public"))
 
-app.listen(PORT,()=>console.log("Site is hosted"))
+db.sync()
+.then(()=>{
+    app.listen(PORT,()=>console.log("Site is hosted"))
+})
+.catch((err)=>{
+    console.error(err)
+})

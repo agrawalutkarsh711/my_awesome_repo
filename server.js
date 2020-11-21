573b39f5192f89e682cabc0c201b44902f0e478b
unknown <agrawalutkarsh711@gmail.com>
2020-11-21 16:38:02 +0530

    Finally deploying on heroku
    Signed-off-by: unknown <agrawalutkarsh711@gmail.com>

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const PORT = process.env.PORT || 4444
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('HELLO TO ALL')
+})
+
+app.use('/public',express.static(__dirname + "./public"))
+
+app.listen(PORT,()=>console.log("Site is hosted"))
